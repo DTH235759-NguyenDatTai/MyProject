45973cb445e94654395f13bf906e9e60c20fd1ad
Kent1 <kent12710@gmail.com>
2025-12-22 00:04:42 +0700

    hehe

--- main/src/main/java/account/regisService.java
+++ main/src/main/java/account/regisService.java
@@ -1,5 +1,8 @@
 package account;
 
 public class regisService {
-
+    public boolean checkRegis(String name, String email, String password){
+        String sql = "INSERT INTO";
+        
+    }
 }
