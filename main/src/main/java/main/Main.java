36c4e987def65e38b8339e928108c82507cd69d4
Kent1 <kent12710@gmail.com>
2025-12-24 02:13:51 +0700

    làm nhẹ

--- main/src/main/java/main/Main.java
+++ main/src/main/java/main/Main.java
@@ -13,22 +13,31 @@ public class Main extends JFrame{
         setTitle("Hệ thống quản lý Siêu Thị");
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null); 
-        setLayout(new GridBagLayout());
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
 
+        JPanel headerPanel = new JPanel();
+        headerPanel.setBackground(new Color(220, 220, 220));
+        BorderLayout layout = new BorderLayout();
+        layout.setHgap(10);
+        layout.setVgap(10);
+        headerPanel.setLayout(layout);
+        
         JButton button = new JButton("Đăng nhập");
         button.setPreferredSize(new Dimension(100, 80));
         button.setBackground(new Color(0, 12,123));
         button.setForeground(new Color(255, 255, 255));
-        button.addActionListener(new ActionListener() {
 
+        headerPanel.add(button, BorderLayout.EAST);
+        button.addActionListener(new ActionListener() {
+            
             @Override
             public void actionPerformed(ActionEvent e) {
                 Login login = new Login();
                 login.setVisible(true);
             }
         });
-        add(button);
+        add(headerPanel, BorderLayout.NORTH);
     }
     public static void main(String[] args) {
         // Sử dụng SwingUtilities để đảm bảo luồng giao diện (UI Thread) chạy ổn định
